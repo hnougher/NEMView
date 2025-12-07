30fd767e3faeb20b2e263c4231440291a45cae3f
Hugh Nougher <hughnougher@gmail.com>
2025-12-07 11:53:04 +1100

    Adding PDPASA RegionalSolution Data, but it doesnt match well

--- gather/pdpasa.sh
+++ gather/pdpasa.sh
@@ -6,7 +6,7 @@
 echo '-----------------------'
 
 # Rotate the log file if larger than 1MB
-logfile=/usr/local/lsws/social-japan.bnr.la/gather/p5_reports.log
+logfile=/usr/local/lsws/social-japan.bnr.la/gather/pdpasa.log
 if [ -f $logfile ]; then
 	if [ $(stat -c%s $logfile) -gt 1048576 ]; then
 		echo 'Rotating log file' + $(stat -c%s $logfile)
@@ -18,19 +18,18 @@ fi
 START_EPOCH=$(date +%s)
 working_dir=/usr/local/lsws/social-japan.bnr.la/tmp
 
-# Sleep for 20 seconds to give AEMO more time to update their website
-sleep 20
+# No delay needed for 30 minute data
 
 # Collect the filenames to download
-previous=$(psql -h 10.240.0.165 -U nem_worker -d nem -t -c "SET TIME ZONE '+10';SELECT to_char(MAX(run_datetime),'YYYYMMDDHH24MI') FROM p5_regionsolution;")
+previous=$(psql -h 10.240.0.165 -U nem_worker -d nem -t -c "SET TIME ZONE '+10';SELECT to_char(MAX(run_datetime),'YYYYMMDDHH24MI') FROM pdpasa_regionsolution;")
 previous=${previous#*$'\n'}
 echo "Previous: $previous"
 
 # Get list of filenames from the AEMO website, and remove any that the middle number is less than or equal to $previous
-rm $working_dir/filenames_p5_reports.html -f
-wget -O $working_dir/filenames_p5_reports.html http://nemweb.com.au/Reports/CURRENT/P5_Reports/
-filenames=$(grep -o "/PUBLIC_P5MIN_[0-9]*_[0-9]*" $working_dir/filenames_p5_reports.html | sed 's#^/##' | awk -v previous="$previous" -F_ '$3 > previous {print}')
-rm $working_dir/filenames_p5_reports.html -f
+rm $working_dir/filenames_pdpasa.html -f
+wget -O $working_dir/filenames_pdpasa.html http://nemweb.com.au/Reports/CURRENT/PDPASA/
+filenames=$(grep -o "/PUBLIC_PDPASA_[0-9]*_[0-9]*" $working_dir/filenames_pdpasa.html | sed 's#^/##' | awk -v previous="$previous" -F_ '$3 > previous {print}')
+rm $working_dir/filenames_pdpasa.html -f
 
 # Only keep the last filename, since we do not need history for now
 # I couldn't get a direct index to work so... loop it is!
@@ -41,7 +40,7 @@ done
 filenames=($final_file)
 
 # Cleanup just in case
-rm $working_dir/PUBLIC_P5MIN_* -f
+rm $working_dir/PUBLIC_PDPASA_* -f
 
 # Loop through the files
 for filename in $filenames
@@ -50,7 +49,7 @@ do
 	# Retry on wget error in case AEMO has a server missing files again
 	COUNT=10
 	while [ $COUNT -gt 0 ]; do
-		if wget -O $working_dir/$filename.zip http://nemweb.com.au/Reports/CURRENT/P5_Reports/$filename.zip; then
+		if wget -O $working_dir/$filename.zip http://nemweb.com.au/Reports/CURRENT/PDPASA/$filename.zip; then
 			break
 		fi
 		COUNT=$((COUNT - 1))
@@ -60,20 +59,20 @@ do
 
 	## REGIONSOLUTION table
 	# Process the file part 1
-	# Remove everything before a line starting with "I,P5MIN,REGIONSOLUTION,", keeping the line
-	# And remove everything from a line starting with "I," onwards
-	awk '/^I,P5MIN,REGIONSOLUTION,/{flag=1;print;next}/^I,/{flag=0}flag' $working_dir/$filename.CSV > $working_dir/$filename.processed.CSV
+	# Remove everything before a line starting with "I,PDPASA,REGIONSOLUTION,", keeping the line
+	# And remove everything from a line starting with "C," onwards
+	awk '/^I,PDPASA,REGIONSOLUTION,/{flag=1;print;next}/^C,/{flag=0}flag' $working_dir/$filename.CSV > $working_dir/$filename.processed.CSV
 
 	# Process the file part 2
 	# Remove the first 4 columns of the CSV
-	mlr --csv cut -x -f "I,P5MIN,REGIONSOLUTION,10" $working_dir/$filename.processed.CSV > $working_dir/$filename.final.CSV
+	mlr --csv cut -x -f "I,PDPASA,REGIONSOLUTION,7" $working_dir/$filename.processed.CSV > $working_dir/$filename.final.CSV
 
-	# Upload the file to the database using the \copy command to insert these new values into the p5_regionsolution table
+	# Upload the file to the database using the \copy command to insert these new values into the pdpasa_regionsolution table
 	psql -h 10.240.0.165 -U nem_worker -d nem <<EOF
 SET TIME ZONE '+10';
 START TRANSACTION;
-TRUNCATE p5_regionsolution;
-\copy p5_regionsolution FROM '$working_dir/$filename.final.CSV' DELIMITER ',' CSV HEADER
+TRUNCATE pdpasa_regionsolution;
+\copy pdpasa_regionsolution FROM '$working_dir/$filename.final.CSV' DELIMITER ',' CSV HEADER
 COMMIT;
 EOF
 
